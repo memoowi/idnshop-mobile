3c2c0b26e70d8d9ef32f5b3f10c83f46fe8d2b91
Misbachul Munir Al Haddad Alwi <megatenlike@gmail.com>
2024-05-30 14:31:56 +0700

    feat: add order page not found

--- lib/src/screens/home_screen_pages/order_page.dart
+++ lib/src/screens/home_screen_pages/order_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:idnshop/src/screens/home_screen_pages/order_page_partials/no_orders_found.dart';
+import 'package:idnshop/src/screens/home_screen_pages/order_page_partials/status_filter.dart';
 import 'package:idnshop/src/theme/custom_color.dart';
 import 'package:idnshop/src/utils/svg_data.dart';
 
@@ -26,7 +28,14 @@ class OrderPage extends StatelessWidget {
           SizedBox(width: 4)
         ],
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            StatusFilter(),
+            NoOrdersFound(),
+          ],
+        ),
+      ),
     );
   }
 }
